ded4151ddaf780a386113e7edd71345847dcca37
AbhishekKr-Jha <akjha4127@gmail.com>
2023-11-07 21:05:49 +0530

    some file changes

--- Conn.js
+++ Conn.js
@@ -12,17 +12,3 @@ mongoose
     console.log("TERMINATED DUE TO__", err);
   });
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://abhishekhp935:OVIdtCoFuj8AiNDq@projecto.pz7xhmb.mongodb.net/Projecto?retryWrites=true&w=majority",
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   )
-//   .then(() => {
-//     console.log("CONNECTTION_SUCCESSFUL");
-//   })
-//   .catch((err) => {
-//     console.log("TERMINATED DUE TO__", err);
-//   });
