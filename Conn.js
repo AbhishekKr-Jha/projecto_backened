d733581937d6002133a07164fd73f2312624858c
AbhishekKr-Jha <akjha4127@gmail.com>
2024-03-15 20:25:30 +0530

    some small updation

--- Conn.js
+++ Conn.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
 
+// mongoose
+//   .connect("mongodb://localhost:27017/PROJECTO", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("CONNECTTION_SUCCESSFUL");
+//   })
+//   .catch((err) => {
+//     console.log("TERMINATED DUE TO__", err);
+//   });
 mongoose
-  .connect("mongodb://localhost:27017/PROJECTO", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+   process.env.CONNECTION_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("CONNECTTION_SUCCESSFUL");
   })
