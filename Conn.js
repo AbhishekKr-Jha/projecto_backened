d34819dc0dc4b52558d6181ce43574b1bcef86d5
AbhishekKr-Jha <akjha4127@gmail.com>
2024-05-15 06:20:13 +0530

    ..

--- Conn.js
+++ Conn.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 //   .catch((err) => {
 //     console.log("TERMINATED DUE TO__", err);
 //   });
-mongoose
+mongoose 
   .connect(
    process.env.CONNECTION_URL,
     {
